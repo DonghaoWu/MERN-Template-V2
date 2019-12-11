a191f234d5cc370ece38934517bdde6d7b314be0
Donghao Wu <donghao.wu.us@gmail.com>
2019-12-10 22:37:53 -0500

    Initialize the app

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 //package
 const express = require('express');
-const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+//Load env vars
+dotenv.config({ path: './config/config.env' })
+
 //apply
 const app = express();
-//middleware
-app.use(express.json({ extended: false }));
+
 //port
 const PORT = process.env.PORT || 5000;
+
+//middleware
+//app.use(express.json({ extended: false }));
+
 
 /*
 DB here!
 */
-connectDB();
 
 /*
 Routes here!!
 */
-app.use("/api", require("./api"));
+app.use('/api', require('./api'));
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT} ===>`));
